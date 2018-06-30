74990d080ea989ee66b3139e6fd78e79f45e3d20
Gustavo Jordão <jordao.f.gustavo@gmail.com>
2018-06-30 22:26:00 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Steps to run the project
+
+## General steps
+
+-   Instal node (recommended version)
+
+## Run Web App
+
+-   Open terminal instance and navigate to Project_directory/web-app
+-   Run “ng serve”
+
+## Run API
+
+-   Open terminal instance and navigate to Project_directory/api
+-   Run “node server
---
+++ api/server.js
@@ -0,0 +1,27 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST request
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;
+
+// API ROUTES
+var router = express.Router();
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get("/", function(req, res) {
+    res.json({ message: "hooray! welcome to our api!" });
+});
+
+// REGISTER ROUTES
+// all routes will be prefixed with /api
+app.use("/api", router);
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log("Magic happens on port " + port);
