f40fdf5eb394c3526a5624be109786a8dddb43da
JoaoMFF <joao_mff@hotmail.com>
2018-07-07 16:54:34 +0100

    Register/Login page creation

--- public/polyfills.js
+++ public/polyfills.js
@@ -5473,7 +5473,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /Users/gustavojordao/Documents/WebProjects/MEAN_Stack/src/polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! C:\Users\Joao_\Documents\Git\MEAN_Stack\src\polyfills.ts */"./src/polyfills.ts");
 
 
 /***/ })
